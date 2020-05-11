82ab027577613c55c06afa0b42ffa54225a9d993
mahinderpartap <53066107+mahinderpartap@users.noreply.github.com>
2020-05-11 18:53:34 +0530

    Add files via upload

---
+++ repeatable read isolation level.sql
@@ -0,0 +1,8 @@
+set transaction isolation level repeatable read 
+begin tran
+select * from tblEmployee
+where EmployeeNumber=125
+waitfor delay '00:00:10'
+select * from tblEmployee
+where EmployeeNumber=125
+rollback
